6989a59c60de751b5222380b3980728f7ecbfb9b
Alex Lancaster <alexl@wi.mit.edu>
2013-11-13 02:05:06 -0500

    Add missing variable to fix crash.  Not sure if this is the best fix, as there is code duplication: "bg_freq_*" are already defined at the top, make global variables?

--- web/lib/server.rb
+++ web/lib/server.rb
@@ -433,6 +433,7 @@ class Server < Sinatra::Base
     
     # generate details
     if !bgfreq_filename.nil? && params[:bg] != "input_sequence"
+      bg_freq_filename = ->(id){ id.nil? ? nil : "bg_freqs_#{id}.txt" }
       bg_freq_local_filename = ->(id){ id.nil? ? nil : "bg_freqs/#{bg_freq_filename[id]}" }
       job.add_file(path(bg_freq_local_filename[params[:bg]]), working_directory)
       bgfreq_opt = " -B #{bgfreq_filename} "
