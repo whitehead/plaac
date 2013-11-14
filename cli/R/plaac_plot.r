234d13693ba648cbebd79a43753682af77f028b0
oliverking <ok@nopales.ad.umassmed.edu>
2013-11-14 16:06:59 -0500

    changed w
    smoothing in plaac was changed, so w was changed here as well. Default
    could be dispensed with entirely by having the plot function look for
    NAs. Mayve later.

--- cli/R/plaac_plot.r
+++ cli/R/plaac_plot.r
@@ -1,9 +1,3 @@
-#!/usr/bin/env Rscript
-## Original program copyright 2009 Whitehead Institute for Biomedial Research
-## Additions copyright 2013 University of Massachusetts
-## Author: Oliver D. King (oliver.king@umassmed.edu) 
-## See LICENSE.TXT for license information.  
-
 args = commandArgs(TRUE)
 source("plaac_plot_util.r") 
 if (length(args) < 2) {
@@ -16,9 +10,11 @@ if (length(args) < 2) {
 } else if (length(args)>2 && args[3]=="-d") {
   plot_genes(args[1], args[2], showSeq=T, showHMMProbs=T, showParses=T, 
              seqUnderline=c("PLAACx2"), seqColor="MAP",
-             tracks=c("FIx2","PLAACx2","PAPAx2","THRESH"), w=41)
+             tracks=c("FIx2","PLAACx2","PAPAx2","THRESH"), w=21)
 } else {
   plot_genes(args[1], args[2], showSeq=T, showHMMProbs=T, showParses=F, 
              seqUnderline=c("FIx2","PAPAx2"), seqColor="MAP",
-             tracks=c("FI","PLAAC","PAPA","THRESH","HYDRO","CHARGE"), w=21)
+             #tracks=c("FI","PLAAC","PAPA","THRESH","HYDRO","CHARGE"), 
+             tracks=c("FI","PLAAC","PAPA","THRESH"),
+             w=1)
 }
