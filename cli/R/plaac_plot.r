894d2e73966b7b3d360637c2e06cdfe0fb3890ee
odking <o.d.king@gmail.com>
2013-12-24 17:26:42 -0500

    truncated protein plots now indicated by jagged grey line
    optional [n] after -c flag contols max length from Rscript

--- cli/R/plaac_plot.r
+++ cli/R/plaac_plot.r
@@ -13,9 +13,11 @@ if (length(args) < 2) {
   cat("   (For png, one file will be output per protein, with a five digit index included in name.)\n")
   cat("   The optional flag -d plots doubly-smoothed rather than singly-smoothed curves.\n")
   cat("   The optional flag -c plots all sequences con one page, color-coded by AA type.\n")
+  cat("   When -c is used, optional integer afterward gives maximum number of AAs to plot per protein.\n")
   cat("See source-code of plaac_plot_util.r and plaac_plot.r for other plot options.\n")
 } else if (length(args)>2 && args[3]=="-c") {
-  color_code_seqs(args[1], args[2], max_n=NA, showLegend=T, showParses=T)
+  max_n = ifelse(length(args) > 3, as.numeric(args[4]), NA) 
+  color_code_seqs(args[1], args[2], max_n=max_n, showLegend=T, showParses=T)
 } else if (length(args)>2 && args[3]=="-d") {
   plot_seqs(args[1], args[2], showSeq=T, showHMMProbs=T, showParses=T, showAAColors=T,
              seqUnderline=c("PLAACx2"), seqColor="MAP",
