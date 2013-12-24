8c4910f4b4e417607622273e609041e84dcb2819
Alex Lancaster <alexl@wi.mit.edu>
2013-12-23 21:04:56 -0500

    Updates for new R API.

--- cli/R/plaac_plot.r
+++ cli/R/plaac_plot.r
@@ -1,13 +1,13 @@
 #!/usr/bin/env Rscript
-## Original program copyright 2009 Whitehead Institute for Biomedial Research
+## Original program copyright 2009 Whitehead Institute for Biomedical Research
 ## Additions copyright 2013 University of Massachusetts
 ## Author: Oliver D. King (oliver.king@umassmed.edu) 
 ## See LICENSE.TXT for license information.  
 
 args = commandArgs(TRUE)
-source("plaac_plot_util.r") 
+source("plaac_plot_util.r")
 if (length(args) < 2) {
-  cat("usage: Rscript plaac_plot.r plotdata.txt figname [-d]\n")
+  cat("usage: Rscript plaac_plot.r plotdata.txt figname [-d|-c]\n")
   cat("   where plotdata.txt is the output of the Java program plaac with the -p option,\n")
   cat("   figname is the name to save the figure as, ending with .pdf or .png.\n")
   cat("   (For png, one file will be output per protein, with a five digit index included in name.)\n")
--- cli/example/visualize_example_output.sh
+++ cli/example/visualize_example_output.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env Rscript
 source("../R/plaac_plot_util.r")
-plot_genes("output.tsv", "output.pdf", showSeq=TRUE)
-plot_genes("output.tsv", "output.png", showSeq=TRUE)
+plot_seqs("output.tsv", "output.pdf", showSeq=TRUE)
+plot_seqs("output.tsv", "output.png", showSeq=TRUE)
